5dad9752ebf6072edf2dc9e66e720ba2d5d248a9
Jeff Marks <jeff.t.marks@gmail.com>
2022-08-28 17:53:11 -0400

    commit tree 0dda0dd893e7c31ba75d121b8fcfb3714b255a76

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
+require "pry"
+
 # Write your code here.
+
+def badge_maker name
+	"Hello, my name is #{name}."
+end
+
+def batch_badge_creator attendees
+	attendees.map { |attendee| badge_maker(attendee) }
+end
+
+def assign_rooms attendees
+	attendees.map.each_with_index do |attendee, i|
+		"Hello, #{attendee}! You'll be assigned to room #{i + 1}!"
+	end
+end
+
+def printer attendees
+	batch_badge_creator(attendees).map { |badge| puts badge }
+	assign_rooms(attendees).map { |assignment| puts assignment }
+end
